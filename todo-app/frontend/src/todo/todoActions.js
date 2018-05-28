4baf7af6b040f32707da982136bb9b8fc5493301
JOAO-GOMES <1171416@isep.ipp.pt>
2018-05-28 03:09:26 +0100

    28/05 changess

---
+++ todo-app/frontend/src/todo/todoActions.js
@@ -0,0 +1,31 @@
+import axios from 'axios'
+
+const URL = 'http://localhost:5005/api/todos'
+
+
+
+export const changeDescription = (event) => ({
+    type: 'DESCRIPTION_CHANGED',
+    payload: event.target.value
+
+})
+
+export const search = () => {
+    const request = axios.get(`${URL}?sort=-createAt`)
+    return {
+        type: 'TODO_SEARCHED',
+        payload: request
+    }
+}
+
+
+export const add = (description) => {
+    return dispatch => {
+        axios.post(URL,{description})
+        .then(resp => dispatch({
+            type: 'TODO_ADDED',
+            payload: resp.data
+    })).then(resp => dispatch(search()))
+    }
+
+}
